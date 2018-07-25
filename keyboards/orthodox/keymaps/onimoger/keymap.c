a68f7dd84cce028c251d9ea57c226851b392cb2e
gediminas <gediminas@iizi.ee>
2018-07-25 23:16:17 +0300

    odox experiment

--- keyboards/orthodox/keymaps/onimoger/keymap.c
+++ keyboards/orthodox/keymaps/onimoger/keymap.c
@@ -45,21 +45,21 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_LEFT, KC_DOWN,          KC_UP, KC_RIGHT,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   KC_LSFT, KC_LCTL,           KC_RALT, KC_SPC, RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT \
+    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_LGUI, TG(_LOWER),       TG(_RAISE), KC_DEL,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   KC_LSFT, KC_LCTL,           KC_RALT, KC_SPC, RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT \
   ),
 
   [_LOWER] = LAYOUT( \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                                KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_LCTL, _______,          _______, KC_RCTL,          KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______,          _______, _______, _______, KC_F12,  KC_HOME,    KC_COMM,    KC_DOT,  KC_END,  _______ \
+    KC_TILD, KC_EXLM, KC_AT,   KC_UNDS, KC_LPRN,  KC_RPRN,                                                                KC_PLUS,    KC_7,    KC_8,   KC_9, KC_MINUS, _______, \
+    KC_HASH, KC_DLR,  KC_PERC, KC_PIPE, KC_LCBR,  KC_RCBR,        _______, _______,             _______, _______,         KC_EQL,     KC_4,    KC_5,   KC_6, KC_ASTR,  XXXXXXX, \
+    KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_LBRC,  KC_RBRC,  _______, _______, _______,        _______, _______, _______,  KC_0,       KC_1,    KC_2,   KC_3, _______,  _______ \
   ),
 
   [_RAISE] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            _______, _______,          _______, _______,          KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______,          _______, _______, _______, KC_F12,  KC_PGUP, KC_COMM, KC_DOT,  KC_PGDN, _______ \
+    KC_MNXT, KC_MPLY,  KC_HOME, KC_UP,    KC_END,   KC_PGUP,                                                                 KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, \
+    KC_VOLD, KC_VOLU,  KC_LEFT, KC_DOWN,  KC_RIGHT, KC_PGDN,           _______, _______,          _______, _______,          KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, \
+    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  _______, _______, _______,          _______, _______, _______, KC_F11,   KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, _______ \
   ),
 
   [_ADJUST] =  LAYOUT( \
