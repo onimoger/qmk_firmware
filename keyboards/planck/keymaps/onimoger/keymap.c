a27254eb166b53c7fc3b1872388c985e8b30e1c4
gediminas <gediminas@iizi.ee>
2018-07-30 21:43:59 +0300

    little changes

--- keyboards/planck/keymaps/onimoger/keymap.c
+++ keyboards/planck/keymaps/onimoger/keymap.c
@@ -51,24 +51,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,      KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,      KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT},
   {KC_NO,   KC_Z,    KC_X,      KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, TG(LOWER), KC_LALT, LOWER,  KC_LSFT, KC_SPC,  RAISE,   KC_DEL,  TG(RAISE), KC_NO,   KC_NO  }
+  {KC_LCTL, KC_LGUI, TG(_LOWER), KC_LALT, LOWER,  KC_LSFT, KC_SPC,  RAISE,   KC_DEL,  TG(_RAISE), KC_NO,   KC_NO  }
 },
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   _  |  (   |   )  |  +   |   7  |   8  |   9  |  =   |      |
+ * |   ~  |   !  |   (  |   )  |  _   |   @  |  +   |   7  |   8  |   9  |  =   |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   #  |   $  |   %  |   |  |  {   |   }  |  -   |   4  |   5  |   6  |  *   |  .   |
+ * |   #  |   $  |   {  |   }  |  |   |  %   |  -   |   4  |   5  |   6  |  *   |  .   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   ^  |   &  |   `  |   \  |  [   |   ]  |  0   |   1  |   2  |   3  |      |      |
+ * |   ^  |   &  |   [  |   ]  |   `  |  \   |  0   |   1  |   2  |   3  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_UNDS, KC_LPRN, KC_RPRN, KC_PLUS, KC_7,    KC_8,    KC_9,    KC_EQL,  _______},
-  {KC_HASH, KC_DLR,  KC_PERC, KC_PIPE, KC_LCBR, KC_RCBR, KC_MINS, KC_4,    KC_5,    KC_6,    KC_ASTR, KC_DOT },
-  {KC_CIRC, KC_AMPR, KC_GRV,  KC_BSLS, KC_LBRC, KC_RBRC, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______},
+  {KC_TILD, KC_EXLM, KC_LPRN, KC_RPRN, KC_UNDS, KC_AT,   KC_PLUS, KC_7,    KC_8,    KC_9,    KC_EQL,  _______},
+  {KC_HASH, KC_DLR,  KC_LCBR, KC_RCBR, KC_PIPE, KC_PERC, KC_MINS, KC_4,    KC_5,    KC_6,    KC_ASTR, KC_DOT },
+  {KC_CIRC, KC_AMPR, KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
